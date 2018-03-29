103b5e635f0d6ad5fae1f887db3d8401d4b20a85
transform80 <yash.dani.98@gmail.com>
2018-03-29 13:40:23 +0530

    Card UI

--- app/src/main/java/com/dcnproject/yashdani/chipin/LoginActivity.java
+++ app/src/main/java/com/dcnproject/yashdani/chipin/LoginActivity.java
@@ -1,66 +1,28 @@
 package com.dcnproject.yashdani.chipin;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText mLoginEmailField;
-    private EditText mLoginPasswordField;
-    private Button mLoginBtn;
+    private Button mLogin;
     private TextView mSignup;
-
-
-    private SharedPreferences mPreferences;
-
-    private FirebaseAuth mAuth;
-
-    private ProgressDialog mProgress;
-    private DatabaseReference mDatabaseRef;
-    private FirebaseDatabase mDatabase;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        mDatabase = FirebaseDatabase.getInstance();
-        mAuth = FirebaseAuth.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users");
-        mDatabaseRef.keepSynced(true);
-        mProgress= new ProgressDialog(this);
-
-        mLoginPasswordField = (EditText) findViewById(R.id.loginPasswordField);
-        mLoginEmailField = (EditText) findViewById(R.id.loginEmailField);
-        mLoginBtn = (Button) findViewById(R.id.loginButton);
+        mLogin=(Button) findViewById(R.id.loginButton);
         mSignup=(TextView) findViewById(R.id.signup);
-
-
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+        mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                checkLogin();
+            public void onClick(View view) {
+                Intent logIntent = new Intent(LoginActivity.this,HomePageWithNav.class);
+                logIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(logIntent);
 
             }
         });
@@ -69,78 +31,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent signIntent = new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(signIntent);
-                finish();
             }
         });
-
-
-
-
-    }
-
-
-
-    private void checkLogin() {
-
-        final String email = mLoginEmailField.getText().toString().trim();
-        final String password = mLoginPasswordField.getText().toString().trim();
-
-        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
-
-            mProgress.setMessage("Checking Login..");
-            mProgress.show();
-
-            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-
-                    if (task.isSuccessful()){
-                        mProgress.dismiss();
-                        checkUserExist();
-                    }
-                    else{
-                        mProgress.dismiss();
-                        Toast.makeText(LoginActivity.this, "Error Login", Toast.LENGTH_LONG).show();
-                    }
-                }
-
-            });
-
-        }
-    }
-
-
-    private void checkUserExist() {
-
-        final String user_id = mAuth.getCurrentUser().getUid();
-        mDatabaseRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.hasChild(user_id)) {
-
-                    Intent mainIntent = new Intent(LoginActivity.this, HomePageWithNav.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(mainIntent);
-
-                    Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_LONG).show();
-                    finish();
-
-
-                }
-                else{
-                    Intent setupIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                    setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(setupIntent);
-
-                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-
-
     }
 }
