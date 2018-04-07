d489abc7a45cc04e97f8b38a5a67ec69cca7938f
transform80 <yash.dani.98@gmail.com>
2018-04-07 19:16:01 +0530

    FULL UI, some changes on a later note

--- app/src/main/java/com/dcnproject/yashdani/chipin/SplashActivity.java
+++ app/src/main/java/com/dcnproject/yashdani/chipin/SplashActivity.java
@@ -5,18 +5,19 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 
-    private static int SPLASH_TIME_OUT=3;
-    private TextView mText;
+    private static int SPLASH_TIME_OUT=3000;
+    private ImageView mImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
-        mText=(TextView) findViewById(R.id.splashtext);
+        mImage=(ImageView) findViewById(R.id.logotimepass);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
